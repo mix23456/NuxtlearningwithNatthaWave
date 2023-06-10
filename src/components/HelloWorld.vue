f096013627ab9f8a4553a6f0531441f6472f7cff
Ponlawat <mixsax2@gmail.com>
2023-06-10 19:01:45 +0700

    header 001

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,38 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
+  <div class="thebox row">
+    <!-- <img class="logo" src="https://cdn1.codashop.com/S/content/mobile/images/codashop-logo-new-3a.png"> -->
+    <div class="textlogo col"><i>วิธีที่เร็วที่สุดและง่ายที่สุดในการซื้อเครดิตเกม</i></div>
   </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<script>
+</script>
+
+<style>
+.textlogo {
+  font-size: 14px;
+  color: white;
+}
+
+.logo {
+  padding: 8px;
+}
+
+.thebox {
+  background-color: #513f55;
+  border-color: #cac5e8;
+  border-style: solid;
+  border-top: 0px;
+  border-left: 0px;
+  border-right: 0px;
+  border-width: 1px;
+  overflow: hidden;
+
+}
+
+.textinthebox {
+  color: white;
+  text-align: center;
+
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './style.css'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.js'
+
+createApp(App).use(bootstrap).mount('#app')
